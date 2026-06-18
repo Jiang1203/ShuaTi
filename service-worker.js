245fd8d0d9c752b50528a34a45ec68b1b99df3f7
Jiang1203 <88334418+Jiang1203@users.noreply.github.com>
2026-06-18 16:00:45 +0800

    发布宠物营养师刷题助手

---
+++ service-worker.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = "pet-nutrition-quiz-v3";
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.webmanifest",
+  "./assets/lucide.min.js",
+  "./assets/icon/app-icon.svg",
+  "./assets/icon/app-icon-192.png",
+  "./assets/icon/app-icon-512.png",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))),
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+
+  const url = new URL(event.request.url);
+  if (url.origin !== self.location.origin) return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request).then((cached) => cached || caches.match("./index.html"))),
+  );
+});
